fb6b39729882deb38d11d47f5b3fb5bde040e6b2
Allan Prellwitz <95494253+Allan-Prellwitz@users.noreply.github.com>
2021-12-08 12:17:50 -0300

    Update Projeto Nota Aluno.c

--- Projeto Nota Aluno.c
+++ Projeto Nota Aluno.c
@@ -6,19 +6,19 @@
 int main()
 {
     setlocale(LC_ALL, "Portuguese");
-    int contador, contador_alunos, x, y, z; //contadores
+    int contador_bimestre, contador_alunos, quantidade, y, z; //contadores
     float media[y], nota[y][3];
-    char resposta, nome_lista[y][100];
+    char resposta, nome_lista[y][50];
 
     do{
     //Quantidade de alunos e os seus nomes
         printf("Quantidade de alunos: ");
-        scanf("%d", &x);
-        if (x==0)
+        scanf("%d", &quantidade);
+        if (quantidade==0)
             return 0;
         printf("\n");
         contador_alunos=1;
-        for(y=0; y<x; y++)
+        for(y=0; y<quantidade; y++)
         {
             printf("Digite o nome do aluno %d: ", contador_alunos);
             scanf("%s[^\n]", nome_lista[y]);
@@ -26,30 +26,30 @@ int main()
             printf("\n");
             //Entrada das respectivas notas
             z=0;
-            contador=1;
+            contador_bimestre=1;
             do
             {
-                printf("Digite a nota bimestre %d: ", contador);
+                printf("Digite a nota bimestre %d: ", contador_bimestre);
                 scanf("%f", &nota[y][z]);
                 z++;
-                contador++;
+                contador_bimestre++;
             }while(z<4);
         printf("\n");
         contador_alunos++;
         }
 
-    //Cálculo das médias
+    //CÃ¡lculo das mÃ©dias
         y=0;
         do
             {
                 media[y]=((nota[y][0]+nota[y][1]+nota[y][2]+nota[y][3])/4);
                 y++;
-            }while(y<x);
+            }while(y<quantidade);
         y=0;
         system("cls");
 
     //Tabela Final
-        printf("Nome\t\tNota 1\tNota 2\tNota 3\tNota 4\tMédia\tResultado");
+        printf("Nome\t\tNota 1\tNota 2\tNota 3\tNota 4\tMÃ©dia\tResultado\n");
         do
             {
                 printf("\n%s\t\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t", nome_lista[y], nota[y][0], nota[y][1], nota[y][2], nota[y][3], media[y]);
@@ -58,7 +58,7 @@ int main()
                 else
                     printf("Reprovado");
                 y++;
-            }while(y<x);
+            }while(y<quantidade);
         printf("\nDeseja iniciar um novo arquivo?\n<s/n>:");
         scanf("%s", &resposta);
         system("cls");
